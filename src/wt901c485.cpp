a29d977b0411d54d26f8173433c8a2cf89f01400
k-trash <k.trash.t@gmail.com>
2024-09-12 18:00:53 +0900

    remove unnecessary file sand sentences

--- src/wt901c485.cpp
+++
@@ -1,7 +0,0 @@
-#include "witmotion_wt901/wt901c485.hpp"
-
-#include <serial_connect/serial_connect.hpp>
-
-#include <rclcpp/rclcpp.hpp>
-#include <sensor_msgs/msg/imu.hpp>
-#include <sensor_msgs/msg/magnetic_field.hpp>
